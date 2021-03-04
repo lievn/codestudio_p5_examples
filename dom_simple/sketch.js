5ddfae4ddb8f964bcdde749d86a4057220bf54b5
Frederik De Bleser <frederik@debleser.be>
2021-03-04 17:17:00 +0100

    Typo fix.

--- dom_simple/sketch.js
+++ dom_simple/sketch.js
@@ -2,21 +2,21 @@
 
 var lengte = 1.82;
 var gewicht = 120;
-var weightslider, lenghtslider;
+var weightslider, lengthSlider;
 
 function setup() {
 	createCanvas(400, 400);
 	textSize(20);
 	weightslider = createSlider(0, 150, 75);
 	weightslider.position(width / 2, 40);
-	lenghtslider = createSlider(0, 220, 180);
-	lenghtslider.position(width / 2, 60);
+	lengthSlider = createSlider(0, 220, 180);
+	lengthSlider.position(width / 2, 60);
 }
 
 function draw() {
 	background(2, 76, 104);
 	gewicht = weightslider.value();
-	lengte = lenghtslider.value() / 100.0;
+	lengte = lengthSlider.value() / 100.0;
 	var BMI = gewicht / (pow(lengte, 2));
 	fill(255, 183, 0);
 	rect(0, height / 3, width, height / 3);
