467fc02ec38b68e6d26b612976f6f6ea922adb1a
Lievn Menschaert <lievenmenschaert@gmail.com>
2021-03-10 18:46:34 +0100

    add mouseIsPressed condition to sketchpad example

--- 04d_basic_variables_sysvar_sketchpad/sketch.js
+++ 04d_basic_variables_sysvar_sketchpad/sketch.js
@@ -1,20 +1,22 @@
 ///* // Variables sketchpad
 
 function setup() {
-    createCanvas(400, 400);
-    background(7, 118, 160);
-    text("beweeg de muis over het canvas om te tekenen", 20, 20);
-    text("klik om opnieuw te beginnen", 20, 40);
+	createCanvas(400, 400);
+	background(7, 118, 160);
+	text("beweeg de muis over het canvas om te tekenen", 20, 20);
+	text("klik een toets om te herstarten", 20, 40);
 }
 
 function draw() {
-    stroke(0, 140, 230);
-    strokeWeight(4.0);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+	stroke(0, 140, 230);
+	strokeWeight(4.0);
+	if (mouseIsPressed) {
+		line(mouseX, mouseY, pmouseX, pmouseY);
+	}
 }
 
-function mousePressed() {
-    background(7, 118, 160);
+function keyPressed() {
+	background(7, 118, 160);
 }
 
 //*/
