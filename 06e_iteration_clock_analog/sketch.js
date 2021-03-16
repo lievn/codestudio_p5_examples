015036f7a27bfb364eda4cf2b335cc6b9e704d89
Frederik De Bleser <frederik@debleser.be>
2021-03-16 20:51:34 +0100

    Update clock example to make it more readable.

--- 06e_iteration_clock_analog/sketch.js
+++ 06e_iteration_clock_analog/sketch.js
@@ -1,59 +1,71 @@
 function setup() {
-	createCanvas(400, 400);
-	textAlign(CENTER);
-	frameRate(100);
+    createCanvas(400, 400);
 }
 
 function draw() {
-	background(2, 76, 104);
+    background(2, 76, 104);
 
-	let s = second();
-	let m = minute();
-	let h = hour();
-	let mi = millis();
+    // For testing: redefine built-int functions for second/minute/hour
+    // let second = () => (mouseY / height) * 60;
+    // let minute = () => 30;
+    // let hour = () => 6;
 
-	let ss = s * 6.0;
-	let mm = m * 6.0;
-	let hh = h * 30.0;
-	let mim = mi * 0.36;
+    // Calculates the fraction (value between 0.0-1.0) of the millis/seconds/minutes/hours.
+    const millisFraction = millis() / 1000;
+    const secondsFraction = second() / 60;
+    const minutesFraction = (minute() + secondsFraction) / 60;
+    const hoursFraction = (hour() + minutesFraction) / 12;
 
-	noFill();
-	stroke(255);
-	ellipse(width / 2, height / 2, 40, 40);
-	ellipse(width / 2, height / 2, 200, 200);
+    // Calculate the angles of the hands of the clock.
+    // Note that 0 degrees in p5js points downwards, so we subtract 180 to make them point up.
+    const hoursAngle = hoursFraction * 360 - 180;
+    const minutesAngle = minutesFraction * 360 - 180;
+    const secondsAngle = secondsFraction * 360 - 180;
+    const millisAngle = millisFraction * 360 - 180;
 
-	strokeWeight(3.0);
+    // When we're drawing angles, make sure we're using degrees, not radians.
+    // https://p5js.org/reference/#/p5/angleMode
+    angleMode(DEGREES);
 
-	//secondenwijzer
-	push();
-	translate(width / 2, height / 2);
-	rotate(radians(ss));
-	stroke(0, 140, 230);
-	line(0, -20, 0, -100);
-	pop();
+    // Draw the clock background
+    noFill();
+    stroke(255);
+    ellipse(width / 2, height / 2, 40, 40);
+    ellipse(width / 2, height / 2, 200, 200);
 
-	//minutenwijzer
-	push();
-	translate(width / 2, height / 2);
-	rotate(radians(mm));
-	stroke(255, 182, 0);
-	line(0, -20, 0, -70);
-	pop();
+    // Hours
+    push();
+    translate(width / 2, height / 2);
+    rotate(hoursAngle);
+    stroke(255, 44, 0);
+    strokeWeight(4);
+    line(0, 20, 0, 70);
+    pop();
 
-	//uur wijzer
-	push();
-	translate(width / 2, height / 2);
-	rotate(radians(hh));
-	stroke(255, 44, 0);
-	line(0, -20, 0, -50);
-	pop();
+    // Minutes
+    push();
+    translate(width / 2, height / 2);
+    rotate(minutesAngle);
+    stroke(255, 182, 0);
+    strokeWeight(3);
+    line(0, 20, 0, 100);
+    pop();
 
-	strokeWeight(1.0);
-	push();
-	translate(width / 2, height / 2);
-	rotate(radians(mim));
-	strokeWeight(6.0);
-	stroke(7, 118, 160);
-	line(0, 0, 0, -20);
-	pop();
+    // Seconds
+    push();
+    translate(width / 2, height / 2);
+    rotate(secondsAngle);
+    stroke(0, 140, 230);
+    strokeWeight(1.5);
+    line(0, 20, 0, 100);
+    pop();
+
+    // Milliseconds
+    push();
+    translate(width / 2, height / 2);
+    rotate(millisAngle);
+    strokeWeight(3);
+    stroke(7, 118, 160);
+    line(0, 0, 0, 16);
+    pop();
 }
